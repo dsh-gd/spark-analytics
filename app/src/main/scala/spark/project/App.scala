1a028437e6530bb9376254c2c585e1c555321a19
dsh-gd <email>
2022-04-14 14:04:09 +0300

    add Iceberg

--- app/src/main/scala/spark/project/App.scala
+++ app/src/main/scala/spark/project/App.scala
@@ -1,20 +1,32 @@
 package spark.project
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 object App {
   def main(args: Array[String]): Unit = {
 
+    val conf = new SparkConf()
+
+    // conf.set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+    conf.set("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
+    conf.set("spark.sql.catalog.spark_catalog.type", "hive")
+    conf.set("spark.sql.catalog.local", "org.apache.iceberg.spark.SparkCatalog")
+    conf.set("spark.sql.catalog.local.type", "hadoop")
+    conf.set("spark.sql.catalog.local.warehouse", "$PWD/warehouse")
+
     val spark = SparkSession
       .builder()
+      .enableHiveSupport()
       .appName("SimpleApp")
       .master("local[*]")
+      .config(conf)
       .getOrCreate()
 
-    val version = spark.version
-    println(version)
-    
-    println(greeting())
-  }
+    spark.sql("CREATE TABLE IF NOT EXISTS local.db.table (id bigint, data string) USING iceberg")
 
-  def greeting(): String = "Hello, world!"
+    spark.sql("INSERT INTO local.db.table VALUES (1, 'a'), (2, 'b'), (3, 'c');")
+
+    val df = spark.table("local.db.table")
+    df.show()
+  }
 }
